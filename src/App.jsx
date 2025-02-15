be462323f82ec10e9bba37175b3303f8facc08c0
cryo-cell <cryobabbi@gmail.com>
2025-02-15 12:57:35 -0500

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Home from "./pages/home";
+import Home from "./pages/Home";
 import Collection from "./pages/Collection";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
