3331216fd7a5fc4259be2b919eeaf9a1103acf63
cryo-cell <cryobabbi@gmail.com>
2025-06-12 01:11:10 -0400

    config

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,15 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  css:{
+  css: {
     postcss: './postcss.config.js'
   },
-  server: {port:5173},
-  compilerOptions:{
-    outDir:"./dist",
-    assetsDir: './assets'
+  server: { port: 5173 },
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets'
   }
 })
